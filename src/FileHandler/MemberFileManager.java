36433584a9ac82a645e992f8060ac088d9b36036
ChristianHundahl <chri02jx@stud.kea.dk>
2021-05-19 14:07:50 +0200

    Created Testing test directory

--- src/FileHandler/MemberFileManager.java
+++ src/FileHandler/MemberFileManager.java
@@ -1,4 +1,5 @@
 package FileHandler;
+//@Christian
 
 public class MemberFileManager {
     //Methods
--- src/FileHandler/MemberManager.java
+++ src/FileHandler/MemberManager.java
@@ -1,4 +1,5 @@
 package FileHandler;
+//@Christian
 
 public class MemberManager {
     //Methods
