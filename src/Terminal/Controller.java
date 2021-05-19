948aec2ec21740b58c084ec843d8f2b9341ec237
Emilia-Victoria <emiliav.helsted@gmail.com>
2021-05-19 15:04:14 +0200

    first draft of controller created

--- src/Terminal/Controller.java
+++ src/Terminal/Controller.java
@@ -1,4 +1,29 @@
 package Terminal;
 
+import UI.Menu;
+
 public class Controller {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+
+        boolean exit = false;
+
+        while(exit == false){
+            int userInput = menu.fetchUserInput();
+            switch(userInput){
+                case 1:
+                    System.out.println("1");
+                    break;
+                case 2:
+                    System.out.println("2");
+                case 3:
+                    System.out.println("3");
+                case 4:
+                    System.out.println("Exiting program...");
+                    exit = true;
+                    break;
+            }
+        }
+    }
+
 }
