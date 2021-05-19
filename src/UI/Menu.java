36ed6769359de209449df0669ad3e79df58fbf49
Emilia-Victoria <emiliav.helsted@gmail.com>
2021-05-19 14:32:14 +0200

    Method showMainMenu updated

--- src/UI/Menu.java
+++ src/UI/Menu.java
@@ -7,7 +7,11 @@ public class Menu {
     private Scanner in = new Scanner(System.in);
 
     public void showMainMenu(){
-        System.out.println("DolphinExpress");
+        System.out.println("DolphinExpress - Svømmehallen Delfinen");
+        System.out.println("Press 1 to Register new member");
+        System.out.println("Press 2 to Manage an existing member");
+        System.out.println("Press 3 to Manage economics");
+        System.out.println("Press 4 to exit");
     }
 
     public int fetchUserInput (){
