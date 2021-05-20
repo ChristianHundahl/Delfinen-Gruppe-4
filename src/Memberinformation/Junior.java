fd38c860985c983002759b247a5c035b42d3c9a2
ChristianHundahl <chri02jx@stud.kea.dk>
2021-05-20 09:11:29 +0200

    Memberinformation

--- src/Memberinformation/Junior.java
+++ src/Memberinformation/Junior.java
@@ -2,11 +2,23 @@ package Memberinformation;
 //@Emilia
 
 public class Junior extends Membership{
-    private double BASIC_MEMBERSHIP_FEE = 1000;
-
+    private double BASIC_MEMBERSHIP_FEE = 1000; //How to change price of all objects without changing code?
+    //To change membership price: Make file rep. database
+    //Each line junior, senior, passive = has price, gets from file
+    //over60 = calc by class?
+    //User --> can write to file for change price
     public Junior(String activity, double price) {
         super(activity, price);
         this.setActivity("Junior");
+    }
+
+    public double getMembershipFee(){ //Abstract method in Membership, each subclass @Overrides
+        //FileManager
+            //Filereader : file
+                //if junior = return price
+        //FileManager.readprices
+            //FileManager find price for Junior
+        return price;
     }
 
     @Override
--- src/Memberinformation/Member.java
+++ src/Memberinformation/Member.java
@@ -1,19 +1,45 @@
 package Memberinformation;
 //@Emilia
 
-public abstract class Member{
+public class Member{
     private String name;
     private int age;
     private boolean isOver60;
     private boolean isInArrears;
     private String activity;
 
-    public static void member(String name, int age, String activity){
-        name = name;
-        age = age;
+    public static void member(){}
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
     }
 
+    public void setAge(int age) {
+        this.age = age;
+    }
     public int getAge() {
         return age;
+    }
+
+    public void setOver60() {
+        if (age >= 60){
+            isOver60 = true;
+        }
+        else {
+            isOver60 = false;}
+    }
+
+    public void setInArrears(boolean inArrears) {
+        isInArrears = inArrears;
+    }
+
+    public void setActivity(String activity) {
+        this.activity = activity;
+    }
+    public String getActivity() {
+        return activity;
     }
 }
