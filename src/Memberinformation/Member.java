626ddbdb3f0742386399c6355477a9b48ac7a5fa
ChristianHundahl <chri02jx@stud.kea.dk>
2021-05-24 10:38:25 +0200

    Member toString v2

--- src/Memberinformation/Member.java
+++ src/Memberinformation/Member.java
@@ -17,8 +17,8 @@ public class Member{
 
     //TODO: MemberToString
         //Return member name + age + activity
-    public void ToString() {
-
+    public String toString() {
+        return name +" "+ age +" "+ activity;
     }
 
     public Member (){}
