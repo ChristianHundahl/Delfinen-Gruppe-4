ad3922219b50bf857d374067bf5f711f33a08440
ChristianHundahl <chri02jx@stud.kea.dk>
2021-05-24 11:14:35 +0200

    MemberTest comments

--- Testing/Memberinformation/MemberTest.java
+++ Testing/Memberinformation/MemberTest.java
@@ -10,12 +10,14 @@ class MemberTest {
 
     @Test
     void getMembershipFee() throws FileNotFoundException {
+        //ARRANGE
         Membership testMembership = new Senior("senior", 1600);
         Member testMember = new Member("Bob", 59);
         Member testMember2 = new Member("Rob", 61);
         testMember.setActivity(testMembership);
         testMember2.setActivity(testMembership);
 
+        //ACT
         double actual = testMember.getMembershipFee();
         double expectedNoDiscount = 1600;
 
