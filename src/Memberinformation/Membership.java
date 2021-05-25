a96fc934d625272337ece9bb170ec6bc777853e6
ChristianHundahl <chri02jx@stud.kea.dk>
2021-05-25 12:03:55 +0200

    Membership cleaning new methods, toString

--- src/Memberinformation/Membership.java
+++ src/Memberinformation/Membership.java
@@ -3,6 +3,7 @@ package Memberinformation;
 
 import FileHandler.FileManager;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public abstract class Membership {
@@ -18,6 +19,10 @@ public abstract class Membership {
         this.price = price;
         this.fm = new FileManager();
         this.sc = fm.getPriceSc();
+    }
+
+    public String toString(){
+        return price + " " + activity;
     }
 
     public Membership(){}
@@ -37,5 +42,45 @@ public abstract class Membership {
     public double getPrice() {
         return price;
     }
+//Methods:
+    //System.out.println("Press 1 to Show projected income");
+    /*Read nop. of members from file
+        Spilt file into:
+        Junior
+        Senior
+        Passive
+    Multiply each category no. of members w/ category price
+    System.out.println(Result)*/
+    public void showProjectedIncome() throws FileNotFoundException{
+        while(memberlist members : memberlist){
 
+            double projectedIncomeFromJuniors = 0;
+            double projectedIncomeFromSeniors = 0;
+            double projectedIncomeFromPassives = 0;
+
+            if(member.Activity(Junior)) {
+                double incomeFromMember = Double.parseDouble(lineAsArray[1]);
+                projectedIncomeFromJuniors = projectedIncomeFromJuniors + incomeFromMember;
+            }
+
+            if(member.Activity(Senior)) {
+                double incomeFromMember = Double.parseDouble(lineAsArray[1]);
+                projectedIncomeFromSeniors = projectedIncomeFromSeniors + incomeFromMember;
+            }
+            if(member.Activity(Passive)) {
+                double incomeFromMember = Double.parseDouble(lineAsArray[1]);
+                projectedIncomeFromPassives = projectedIncomeFromPassives + incomeFromMember;
+            }
+
+            System.out.println("Projected income form Junior members: " + projectedIncomeFromJuniors);
+            System.out.println("Projected income form Senior members: " + projectedIncomeFromSeniors);
+            System.out.println("Projected income form Passive members: " + projectedIncomeFromPassives);
+
+        }
+    }
+
+    //System.out.println("Press 3 to change prices");
+    /*Read file
+    * Show current prices
+    * Take input to change prices from user*/
 }
